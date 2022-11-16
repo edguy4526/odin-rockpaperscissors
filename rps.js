bab3ebeb2f3ac49709aedfe5d394fb6398eb1eaf
edguy4526 <200~99697360+edguy4526@users.noreply.github.com>
2022-11-16 11:47:52 +0800

    help

--- rps.js
+++ rps.js
@@ -1,24 +1,25 @@
 console.log("Type 'rock', 'paper', or 'scissors' into the prompt to select your choice!")
 function getComputerChoice () {
-    let rps = [
-        "rock",
-        "paper",
-        "scissors"
-    ]
-    let computerChoice = rps[Math.floor(Math.random() * rps.length)]
-    return computerChoice;
+    let computerChoice = Math.floor(Math.random() * 3)
+    if (computerChoice = 0) {
+        return "rock";
+    } else if (computerChoice = 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 function rockPaperScissors (playerSelection, computerSelection) {
-    if (computerSelection == /playerSelection/i) {
+    if (computerSelection === /playerSelection/i) {
         return ("It was a tie!");
-    } else if ((playerSelection == /rock/i && computerSelection == /paper/i) ||
-    (playerSelection == /paper/i && computerSelection == /scissors/i) ||
-    (playerSelection == /scissors/i && computerSelection == /rock/i)) {
+    } else if ((playerSelection === /rock/i && computerSelection == /paper/i) ||
+    (playerSelection === /paper/i && computerSelection === /scissors/i) ||
+    (playerSelection === /scissors/i && computerSelection === /rock/i)) {
         return ("You lose!");
-    } else if ((playerSelection == /paper/i && computerSelection == /rock/i) ||
-    (playerSelection == /scissors/i && computerSelection == /paper/i) ||
-    (playerSelection == /rock/i && computerSelection == /scissors/i)) {
+    } else if ((playerSelection === /paper/i && computerSelection === /rock/i) ||
+    (playerSelection === /scissors/i && computerSelection === /paper/i) ||
+    (playerSelection === /rock/i && computerSelection === /scissors/i)) {
         return ("You win!");
     } else {
         return "Something is broken";
