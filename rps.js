a854b1717cdcbef2492541acceb4204f5f95b02a
edguy4526 <200~99697360+edguy4526@users.noreply.github.com>
2022-11-17 10:52:35 +0800

    i have done literally nothing this sucks

--- rps.js
+++ rps.js
@@ -1,32 +1,28 @@
-console.log("Type 'rock', 'paper', or 'scissors' into the prompt to select your choice!")
+console.log("Type 'rock', 'paper', or 'scissors' into the prompt to select your choice!");
 function getComputerChoice () {
-    let computerChoice = Math.floor(Math.random() * 3)
-    if (computerChoice = 0) {
-        return "rock";
-    } else if (computerChoice = 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+    let computerChoice = Math.floor((Math.random() * (3)) + 1);
+    return computerChoice
 }
-
 function rockPaperScissors (playerSelection, computerSelection) {
-    if (computerSelection === /playerSelection/i) {
+    // 1 = rock, 2 = paper, 3 = scissors
+    if ((computerSelection === 1 && playerSelection === /rock/i) ||
+    (computerSelection === 2 && playerSelection === /paper/i) ||
+    (computerSelection === 3 && playerSelection === /scissors/i)) {
         return ("It was a tie!");
-    } else if ((playerSelection === /rock/i && computerSelection == /paper/i) ||
-    (playerSelection === /paper/i && computerSelection === /scissors/i) ||
-    (playerSelection === /scissors/i && computerSelection === /rock/i)) {
+    } else if ((playerSelection === /rock/i && computerSelection == 2 ||
+    (playerSelection === /paper/i && computerSelection === 3) ||
+    (playerSelection === /scissors/i && computerSelection === 1))) {
         return ("You lose!");
-    } else if ((playerSelection === /paper/i && computerSelection === /rock/i) ||
-    (playerSelection === /scissors/i && computerSelection === /paper/i) ||
-    (playerSelection === /rock/i && computerSelection === /scissors/i)) {
+    } else if ((playerSelection === /paper/i && computerSelection === 1) ||
+    (playerSelection === /scissors/i && computerSelection === 2) ||
+    (playerSelection === /rock/i && computerSelection === 3)) {
         return ("You win!");
     } else {
-        return "Something is broken";
+        return ("Something is broken");
     }
 }
 
-let playerSelection = prompt();
-let computerSelection = getComputerChoice();
+let playerSelection = ("paper");
+let computerSelection = (1);
 
-console.log(rockPaperScissors(playerSelection, computerSelection));
+console.log(rockPaperScissors());
