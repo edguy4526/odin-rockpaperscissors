1540d5c201d6318fa4bbc740e779345cd3e59cbb
edguy4526 <200~99697360+edguy4526@users.noreply.github.com>
2022-11-17 11:59:11 +0800

    base game is done, i almost nuked my laptop with an infinite loop

--- rps.js
+++ rps.js
@@ -5,24 +5,50 @@ function getComputerChoice () {
 }
 function rockPaperScissors (playerSelection, computerSelection) {
     // 1 = rock, 2 = paper, 3 = scissors
-    if ((computerSelection === 1 && playerSelection === /rock/i) ||
-    (computerSelection === 2 && playerSelection === /paper/i) ||
-    (computerSelection === 3 && playerSelection === /scissors/i)) {
-        return ("It was a tie!");
-    } else if ((playerSelection === /rock/i && computerSelection == 2 ||
-    (playerSelection === /paper/i && computerSelection === 3) ||
-    (playerSelection === /scissors/i && computerSelection === 1))) {
-        return ("You lose!");
-    } else if ((playerSelection === /paper/i && computerSelection === 1) ||
-    (playerSelection === /scissors/i && computerSelection === 2) ||
-    (playerSelection === /rock/i && computerSelection === 3)) {
-        return ("You win!");
+    playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'!").toLowerCase();
+    computerSelection = getComputerChoice();
+    if ((computerSelection === 1 && playerSelection === "rock") ||
+    (computerSelection === 2 && playerSelection === "paper") ||
+    (computerSelection === 3 && playerSelection === "scissors")) {
+        return ("It was a tie! The computer picked " + computerSelection + " and you picked " + playerSelection +".");
+    } else if ((playerSelection === "rock" && computerSelection == 2 ||
+    (playerSelection === "paper" && computerSelection === 3) ||
+    (playerSelection === "scissors" && computerSelection === 1))) {
+        computerWins++
+        return ("You lose! The computer picked " + computerSelection + " and you picked " + playerSelection +".");
+    } else if ((playerSelection === "paper" && computerSelection === 1) ||
+    (playerSelection === "scissors" && computerSelection === 2) ||
+    (playerSelection === "rock" && computerSelection === 3)) {
+        playerWins++
+        return ("You win! The computer picked " + computerSelection + " and you picked " + playerSelection +".");
     } else {
+        console.log("The values were " + computerSelection + " and " + playerSelection);
         return ("Something is broken");
     }
 }
 
-let playerSelection = ("paper");
-let computerSelection = (1);
-
-console.log(rockPaperScissors());
+function runRound () {
+    for (let i = 0; i < 10; i++) {
+        console.log(rockPaperScissors(playerSelection, computerSelection))
+        if (playerWins > computerWins) {
+            console.log("The score is " + playerWins + " to " + computerWins + ". You're in the lead!")
+        } else if (computerWins > playerWins) {
+            console.log("The score is " + computerWins + " to " + playerWins + ". The computer's in the lead!")
+        } else if (playerWins === computerWins) {
+            console.log("The score is "  + playerWins + " to " + computerWins + ". It's a tie!")
+        }
+        if (playerWins >= 5) {
+            console.log("YOU WON!!!");
+            break;
+        } else if (computerWins >= 5) {
+            console.log("The computer won...");
+            break;
+        }
+    }
+}
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice();
+let playerWins = 0;
+let computerWins = 0;
+// please don't try using runRound it nukes your computer
+console.log(runRound());
