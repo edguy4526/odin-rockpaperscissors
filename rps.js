8bc2be31ef9a16f96897d6096257905ee75cbb18
edguy4526 <200~99697360+edguy4526@users.noreply.github.com>
2022-11-21 10:43:52 +0800

    the year is 20XX. robots are detecting human plays in rock paper scissors in 1 frame, always winnig 5-0. robot vs robot matches cause both robots to stare into each other's hands for several centuries. human rock paper scissors is now obsolete, making two robots looking at each other the main televised sport. welcome to the future of rock paper scissors AI

--- rps.js
+++ rps.js
@@ -1,35 +1,59 @@
 console.log("Type 'rock', 'paper', or 'scissors' into the prompt to select your choice!");
-function getComputerChoice () {
+
+// generates random object
+function getComputerChoiceRandom () {
     let computerChoice = Math.floor((Math.random() * (3)) + 1);
     return computerChoice
 }
+
+// returns counter to user's last played object
+function getComputerChoiceLastCounter (lastPicked) {
+    if (lastPicked == 1) {
+        return 2;
+    } else if (lastPicked == 2) {
+        return 3;
+    } else if (lastPicked == 3){
+        return 1;
+    } else {
+        return getComputerChoiceRandom();
+    }
+}
+
+// runs one round of RPS
 function rockPaperScissors (playerSelection, computerSelection) {
     // 1 = rock, 2 = paper, 3 = scissors
-    playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'!").toLowerCase();
-    computerSelection = getComputerChoice();
     if ((computerSelection === 1 && playerSelection === "rock") ||
     (computerSelection === 2 && playerSelection === "paper") ||
     (computerSelection === 3 && playerSelection === "scissors")) {
         return ("It was a tie! The computer picked " + computerSelection + " and you picked " + playerSelection +".");
+        let lastPicked = lastPlayed(computerSelection);
     } else if ((playerSelection === "rock" && computerSelection == 2 ||
     (playerSelection === "paper" && computerSelection === 3) ||
     (playerSelection === "scissors" && computerSelection === 1))) {
-        computerWins++
+        computerWins++;
+        let lastPicked = lastPlayed(computerSelection);
         return ("You lose! The computer picked " + computerSelection + " and you picked " + playerSelection +".");
     } else if ((playerSelection === "paper" && computerSelection === 1) ||
     (playerSelection === "scissors" && computerSelection === 2) ||
     (playerSelection === "rock" && computerSelection === 3)) {
         playerWins++
+        let lastPicked = lastPlayed(computerSelection);
         return ("You win! The computer picked " + computerSelection + " and you picked " + playerSelection +".");
     } else {
         console.log("The values were " + computerSelection + " and " + playerSelection);
         return ("Something is broken");
     }
 }
-
+// runs an ft5 game of RPS
 function runRound () {
     for (let i = 0; i < 10; i++) {
-        console.log(rockPaperScissors(playerSelection, computerSelection))
+      if (i = 0) {
+            let playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'!").toLowerCase();
+            console.log(rockPaperScissors(playerSelection, getComputerChoiceRandom()))
+       } else {
+            let playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'!").toLowerCase();
+            console.log(rockPaperScissors(playerSelection, getComputerChoiceLastCounter()))
+        }
         if (playerWins > computerWins) {
             console.log("The score is " + playerWins + " to " + computerWins + ". You're in the lead!")
         } else if (computerWins > playerWins) {
@@ -46,9 +70,21 @@ function runRound () {
         }
     }
 }
-let playerSelection = "Rock";
-let computerSelection = getComputerChoice();
+// returns user's last played object in numerical form
+function lastPlayed (input){
+    let lastPlay;
+    if (input === "rock") {
+        lastPlay = 1;
+    } else if (input === "paper") {
+        lastPlay = 2;
+    } else if (input === "scissors") {
+        lastPlay = 3;
+    } else {
+        lastPlay = getComputerChoiceRandom();
+    }
+    return lastPlay
+}
+
 let playerWins = 0;
 let computerWins = 0;
-// please don't try using runRound it nukes your computer
 console.log(runRound());
